02b4305bfc71f68540d6b4f90d004cf884fd1fd6
troliaoluatsu <thienan041803@gmail.com>
2023-04-27 14:01:58 +0700

    Bo sung quan ly nhan vien

--- QuanLyTourDuLich/src/GiaoDien/QuanLyGiaoDien.java
+++ QuanLyTourDuLich/src/GiaoDien/QuanLyGiaoDien.java
@@ -5,10 +5,36 @@
  */
 package GiaoDien;
 
+import QuanLyNhanVIen.EmployeeManagement;
+import java.util.Scanner;
+
 /**
  *
  * @author DELL
  */
 public class QuanLyGiaoDien {
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        do {
+            System.out.println("\n\nChọn chức năng:");
+            System.out.println("0. Thoát chương trình.");
+            System.out.println("1. Dang nhap vao quan ly nhan vien");
+            System.out.println("Moi nhap lua chon cua ban: ");
+
+            choice = sc.nextInt();
+            sc.nextLine(); // Đọc bỏ dòng trống sau khi đọc số
+            switch (choice) {
+                case 1:
+                    EmployeeManagement.main();
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Vui lòng chọn lại.");
+                    break;
+            }
+        } while (choice != 0);
+        EmployeeManagement.main();
+
+    }
 }
